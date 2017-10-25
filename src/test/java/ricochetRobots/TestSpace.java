4b6d8e1c1d1dd3ae98fa2853a59a3c7fdcd28015
Ben Reynolds <breynolds@mitre.org>
2017-10-25 10:38:50 -0400

    Fix Unit test

--- src/test/java/ricochetRobots/TestSpace.java
+++ src/test/java/ricochetRobots/TestSpace.java
@@ -41,22 +41,22 @@ public class TestSpace {
         assertEquals(s.getWallSouth(), false);
         assertEquals(s.getWallEast(), false);
         assertEquals(s.getWallWest(), false);
-        assesrtEquals(s.hasWall('N'), false);
-        assesrtEquals(s.hasWall('S'), false);
-        assesrtEquals(s.hasWall('E'), false);
-        assesrtEquals(s.hasWall('W'), false);
+        assertEquals(s.hasWall('N'), false);
+        assertEquals(s.hasWall('S'), false);
+        assertEquals(s.hasWall('E'), false);
+        assertEquals(s.hasWall('W'), false);
         s.setWallNorth(true);
         assertEquals(s.getWallNorth(), true);
-        assesrtEquals(s.hasWall('N'), true);
+        assertEquals(s.hasWall('N'), true);
         s.setWallSouth(true);
         assertEquals(s.getWallSouth(), true);
-        assesrtEquals(s.hasWall('S'), true);
+        assertEquals(s.hasWall('S'), true);
         s.setWallEast(true);
         assertEquals(s.getWallEast(), true);
-        assesrtEquals(s.hasWall('E'), true);
+        assertEquals(s.hasWall('E'), true);
         s.setWallWest(true);
         assertEquals(s.getWallWest(), true);
-        assesrtEquals(s.hasWall('W'), true);
+        assertEquals(s.hasWall('W'), true);
     }
 
     @Test
